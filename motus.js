a5ca7e05e570cef59557c3cc79cc12eff19c9f64
clem-0000 <clemtoon78700.bri@gmail.com>
2022-04-16 22:38:16 +0200

    suite fonction récupérer les valeurs saisies par l'utilisateur sans écrire dans un input

--- motus.js
+++ motus.js
@@ -3,12 +3,12 @@ var userWord = [];
 var wordLength = wordToFind.length; // length of the word use for number of input to display
 
 /**
- * récupère les valeurs saisies par l'utilisateur
+ * récupère les valeurs saisies par l'utilisateur -> obsolete
  */
 const getValueSubmit = () => {
-  for (let i = 0; i < wordLength; i++) {
+  /* for (let i = 0; i < wordLength; i++) {
     userWord.push(document.getElementById(i).value);
-  }
+  } */
   console.log(userWord);
 };
 
@@ -32,12 +32,28 @@ const compareLetter = () => {
   // appeler la méthode display input pour afficher une nouvelle salve d'input. faire en sorte d'avoir value = lettre bien placé
 };
 
-document.addEventListener(
-  "keypress",
-  (event) => {
-    var name = event.key;
-    var code = event.code;
-    console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
-  },
-  false
-);
+/**
+ * *permet de récupérer les valeurs alphanumérique [a-Z] saisies par l'utilisateur et l'ajoute à la liste contenant le mot saisie par l'utilisateur
+ */
+window.addEventListener("keydown", (event) => {
+  if (
+    event.key != "" &&
+    event.code != "KeyM" &&
+    userWord.length() < wordLength  
+    (event.code.match("Key") ||
+      event.code.match("Semicolon") ||
+      event.key == "Backspace" ||
+      event.key == "Enter")
+  ) {
+    // remove click value
+    if (event.key == "Backspace") {
+      userWord.pop();
+    } else if (event.key == "Enter") {
+      //call function verifiedWord
+    } else {
+      userWord.push(event.key);
+    }
+    console.log(event);
+  }
+}); // permet de récupérer les touches exécutées
+console.log(userWord);
