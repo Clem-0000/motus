5767ff4fee21ef0b474858fc59c78a0907e91580
clem-0000 <clemtoon78700.bri@gmail.com>
2022-04-17 17:39:22 +0200

    ajout fonction compareLetter et modification récupération valeur saisie par l'utilisateur

--- motus.js
+++ motus.js
@@ -1,6 +1,14 @@
-var wordToFind = "test";
+var wordToFind = "actuel";
+var arrayWordToFind = ["a", "c", "t", "u", "e", "l"];
 var userWord = [];
 var wordLength = wordToFind.length; // length of the word use for number of input to display
+const userChance = 6;
+const colorForTab = {
+  colorBasic: "rgb(2 132 199)",
+  colorWrongValue: "rgb(12 76 112)",
+  colorGoodPlace: "rgb(239 68 68)",
+  colorWrongPlace: "rgb(234 179 8)",
+};
 
 /**
  * récupère les valeurs saisies par l'utilisateur -> obsolete
@@ -14,13 +22,20 @@ const getValueSubmit = () => {
 
 const displayInput = () => {
   var container = document.getElementById("containerInput"); // on met l'ID de la div qui va contenir tous les inputs
-
+  while (container.hasChildNodes()) {
+    container.removeChild(container.lastChild);
+  }
   for (i = 0; i < wordLength; i++) {
     var input = document.createElement("input");
     input.type = "text"; // type de l'input
     input.id = i; // permet d'avoir un id différent pour chaque input pour pouvoir récupérer les valeurs
     input.className = "inputMotus"; // permet de mettre un style sur l'input
     input.maxLength = "1";
+    input.value = userWord[i] != undefined ? userWord[i] : null;
+    input.style.backgroundColor =
+      userWord[i] != undefined
+        ? colorForTab.colorBasic
+        : colorForTab.colorWrongValue;
     container.appendChild(input);
     container.appendChild(document.createElement("br"));
   }
@@ -30,6 +45,14 @@ const displayInput = () => {
 
 const compareLetter = () => {
   // appeler la méthode display input pour afficher une nouvelle salve d'input. faire en sorte d'avoir value = lettre bien placé
+  userWord.forEach((userLetter, index) => {
+    if (arrayWordToFind.includes(userLetter, index)) {
+      console.log(
+        "🚀 ~ file: motus.js ~ line 42 ~ userWord.forEach ~ userLetter",
+        userLetter
+      );
+    }
+  });
 };
 
 /**
@@ -39,21 +62,21 @@ window.addEventListener("keydown", (event) => {
   if (
     event.key != "" &&
     event.code != "KeyM" &&
-    userWord.length() < wordLength  
+    (userWord.length < wordLength || event.key == "Backspace") &&
     (event.code.match("Key") ||
-      event.code.match("Semicolon") ||
       event.key == "Backspace" ||
+      event.code.match("Semicolon") ||
       event.key == "Enter")
+    // remove click value et autre saisie qu'un delete entrer ou une lettre
   ) {
-    // remove click value
     if (event.key == "Backspace") {
       userWord.pop();
     } else if (event.key == "Enter") {
-      //call function verifiedWord
+      //call function compareLetter
     } else {
       userWord.push(event.key);
     }
+    displayInput();
     console.log(event);
   }
-}); // permet de récupérer les touches exécutées
-console.log(userWord);
+});
